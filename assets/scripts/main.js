e8311c7498593a0d9d3f8913eed64e9d5090ff9d
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-25 17:05:57 +0100

    Update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,9 @@ let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+    },
     scene: {
         preload: preload,
         create: create,
