ad158b2899c5eb6527db53dc54e9967ec236f584
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-25 14:41:49 +0100

    Update

---
+++ phaserDemo/assets/scripts/main.js
@@ -0,0 +1,27 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+
+//1)
+function preload() {
+
+}
+
+//2)
+function create() {
+
+}
+
+//3)
+function update() {
+
+}
