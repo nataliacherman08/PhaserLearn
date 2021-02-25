0a1a2d3714ff1812cdb0bcdcf7e986c309eaf8dc
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-25 14:53:21 +0100

    Update

---
+++ assets/scripts/main.js
@@ -0,0 +1,30 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+
+//1) Allows you to preload files (95% of the time they are images) 
+function preload() {
+    this.load.image('octopus', '../assets/img/octopusRight.png');
+}
+
+
+//2) Initialize the game
+function create() {
+
+}
+
+
+//3) Create all the logic of the game
+//(Example, when you press the right arrow, the character goes to the right)
+function update() {
+
+}
