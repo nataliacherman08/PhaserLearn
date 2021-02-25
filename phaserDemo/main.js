352865b626662c9945b74dc959a30f08ee7bdf78
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-25 11:46:22 +0100

    Update

--- phaserDemo/main.js
+++
@@ -1,3 +0,0 @@
-let game = new Phaser.Game(600, 400, Phaser.AUTO);
-game.state.add("state1", demo.state1);
-game.state.start("state1");
--- phaserDemo/state1.js
+++
@@ -1,7 +0,0 @@
-let demo = {};
-demo.state1 = function () { };
-demo.state1.prototype = {
-    preload: function () { },
-    create: function () { },
-    update: function () { }
-};
