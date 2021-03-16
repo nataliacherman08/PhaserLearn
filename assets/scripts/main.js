325a1e2af4ab30c82078e3c52c3975e315425996
nataliacherman08 <cherman.natalia08@gmail.com>
2021-03-16 13:33:11 +0100

    Update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -19,18 +19,15 @@ let game = new Phaser.Game(config);
 let platforms;
 let yellowRaincoat;
 let cursors;
-let stars;
-let score;
-let scoreText;
-let bombs;
+let roses;
 
 //1) Allows you to preload files (95% of the time they are images) 
 function preload() {
     this.load.image("forest", "assets/img/darkBg.png");
     this.load.image("ground", "assets/img/platform.png");
-    this.load.image("star", "assets/img/star.png");
+    this.load.image("rose", "assets/img/rose.png");
     this.load.image("bomb", "assets/img/bomb.png");
-    this.load.image("yellowRaincoat", "assets/img/yellowraincoat.png");
+    this.load.spritesheet("yellowRaincoat", "assets/img/yellowraincoat.png", { frameWidth: 40, frameHeight: 50 });
 }
 
 
@@ -49,6 +46,20 @@ function create() {
     platforms.create(600, 610, "ground").setScale(6).refreshBody();
 
     //Player
+    /*this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("yelllowRaincoat", { start: 0, end: 1 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("yelllowRaincoat", { start: 2, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });*/
+
     yellowRaincoat = this.physics.add.image(110, 100, "yellowRaincoat");
     yellowRaincoat.setCollideWorldBounds(true);
     this.physics.add.collider(yellowRaincoat, platforms);
@@ -59,69 +70,50 @@ function create() {
     cursors = this.input.keyboard.createCursorKeys();
 
     //Items
-    stars = this.physics.add.group({
+    roses = this.physics.add.group({
 
-        key: "star",
+        key: "rose",
 
         repeat: 12,
 
         setXY: { x: 12, y: 0, stepX: 70 }
 
     });
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.overlap(yellowRaincoat, stars, collectStar, null, this);
+    this.physics.add.collider(roses, platforms);
+    this.physics.add.overlap(yellowRaincoat, roses, collectRoses, null, this);
 
-    stars.children.iterate(function (child) {
+    roses.children.iterate((child) => {
 
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
     });
-
-    //Score
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#fff' });
-
-    //Bombs
-    bombs = this.physics.add.group();
-    this.physics.add.collider(bombs, platforms);
 }
 
 
-//
-function collectStar(yellowRaincoat, star) {
+//Function to collect roses
+function collectRoses(yellowRaincoat, rose) {
 
-    star.disableBody(true, true);
+    rose.disableBody(true, true);
 
+    if (roses.countActive(true) === 0) {
 
-    if (stars.countActive(true) === 0) {
-
-        stars.children.iterate(child => {
+        roses.children.iterate(child => {
 
             child.enableBody(true, child.x, 0, true, true);
         });
     }
 
-    score += 10;
-    scoreText.setText("Score :" + score);
-
-    if (stars.countActive(true) === 0) {
-        var x = (yellowRaincoat.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = false;
+    if (roses.countActive(true) === 0) {
+        let x = (yellowRaincoat.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
     }
 }
 
 //3) Create all the logic of the game
 //(Example, when you press the right arrow, the character goes to the right)
 function update() {
-
-
     // Allows to stop the movement of the character
-    /*yellowRaincoat.setVelocityX(0);
-    yellowRaincoat.setVelocityY(0);*/
+    yellowRaincoat.setVelocityX(0);
+    //yellowRaincoat.setVelocityY(0);
 
     // If the key is pressed "isDown", then the velocity interacts with gravity
     if (cursors.up.isDown) {
@@ -134,9 +126,11 @@ function update() {
 
     if (cursors.right.isDown) {
         yellowRaincoat.setVelocity(300, 0);
+        //yellowRaincoat.anims.play("right", true);
     }
 
     if (cursors.left.isDown) {
         yellowRaincoat.setVelocity(-300, 0);
+        //yellowRaincoat.anims.play("left", true);
     }
 }
